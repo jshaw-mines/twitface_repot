7186461ca7123f560417e7956b03a2d58ff8e4af
Joshua Shaw <jshaw@mines.edu>
2013-03-23 14:53:39 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitfaceRepot::Application.config.secret_token = 'd06e2a39b243a6c02c42f37a1f028c907571d30b8c8c30d8ff1bd57bdbdb8119a6f559ca0550bf53f5e3ae76d965f304c7ee3665c01dd3688db890b9f6e5e0b0'
